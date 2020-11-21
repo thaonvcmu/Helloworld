88c84668075b2e10b8e4311cd7aed352ccad1b0c
thaonv <thaonvcmu74@gmail.com>
2020-11-21 22:42:34 +0700

    commit 21/11

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,13 +3,27 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //12345dasd
+        //The arrayAdatper is the controller in our
+        //model-view-controller relationship. (controller)
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                //context
+                this,
+                //layout (view)
+                R.layout.reminders_row,
+                //row (view)
+                R.id.row_text,
+                //data (model) with bogus data to test our listview
+                new String[]{"first record", "second record", "third record"});
+        mListView.setAdapter(arrayAdapter);
     }
 }
